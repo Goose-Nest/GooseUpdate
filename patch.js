214606c8bf4f4e07d73f7dd7422f093cbb9f31c1
Oj <oj@oojmed.com>
2020-11-28 18:54:32 +0000

    Patch v10: Don't override (some) other mod\'s CSP disabler with the patch\'s CSP unrestrictor

--- patch.js
+++ patch.js
@@ -1,44 +1,52 @@
 (async function() {
-  const version = 9;
+  const version = 10;
 
-  function rgb(r, g, b, text) {
-    return `\x1b[38;2;${r};${g};${b}m${text}\x1b[0m`;
-  }
+  const unstrictCSP = () => {
+    log('Setting up CSP unstricter...');
 
-  function log(msg) {
-    console.log(`[${rgb(250, 250, 0, 'GooseMod')}] ${msg}`);
-  }
+    const cspAllowAll = [
+      'connect-src',
+      'style-src',
+      'img-src'
+    ];
+
+    electron.session.defaultSession.webRequest.onHeadersReceived(({ responseHeaders }, done) => {
+      let csp = responseHeaders['content-security-policy'];
+
+      if (csp) {
+        for (let p of cspAllowAll) {
+          csp[0] = csp[0].replace(`${p}`, `${p} *`);
+        }
+
+        // Fix Discord's broken CSP which disallows unsafe-inline due to having a nonce (which they don't even use?)
+        csp[0] = csp[0].replace(/'nonce-.*?' /, '');
+      }
+
+      done({ responseHeaders });
+    });
+  };
+
+  const rgb = (r, g, b, text) => `\x1b[38;2;${r};${g};${b}m${text}\x1b[0m`;
+
+  const log = function() { console.log(`[${rgb(250, 250, 0, 'GooseMod')}]`, ...arguments); }
 
   const electron = require('electron');
 
+  const otherMods = {
+    specific: {
+      betterDiscord: electron.app.commandLine.hasSwitch("no-force-async-hooks-checks"), // BetterDiscord adds a command line switch in it's injector
+      // powercord: (new electron.BrowserWindow({webContents: {}})).webContents.__powercordPreload !== undefined, // PowerCord adds a property to BrowserWindow's webContent's
+    },
+    generic: {
+      electronProxy: require('util').types.isProxy(electron) // Many modern mods overwrite electron with a proxy with a custom BrowserWindow (copied from PowerCord)
+    }
+  };
+
   log(`GooseUpdate Desktop Core Module Patch - Version ${version}`);
 
-  log('Setting up CSP disabler...');
+  log(otherMods);
 
-  const cspAllowAll = [
-    'connect-src',
-    'style-src',
-    'img-src'
-  ];
-
-  electron.session.defaultSession.webRequest.onHeadersReceived(({ responseHeaders }, done) => {
-    let csp = responseHeaders['content-security-policy'];
-
-    if (csp) {
-      for (let p of cspAllowAll) {
-        csp[0] = csp[0].replace(`${p}`, `${p} *`);
-      }
-
-//      console.log(csp[0]);
-
-      // Fix Discord's broken CSP which disallows unsafe-inline due to having a nonce (which they don't even use?)
-      csp[0] = csp[0].replace(/'nonce-.*?' /, '');
-
-//      console.log(csp[0]);
-    }
-
-    done({ responseHeaders });
-  });
+  if (!otherMods.generic.electronProxy) unstrictCSP();
 
   let i = setInterval(() => {
     log('Attempting to get main window');
@@ -54,7 +62,7 @@
     bw.webContents.on('dom-ready', () => {
       log('dom-ready triggered: injecting GooseMod JS');
 
-      // bw.webContents.executeJavaScript(`(async function() { alert(1); }).bind(window)();`);
+      // bw.webContents.executeJavaScript(require('fs').readFileSync('/home/duck/GooseMod/Injector/dist/index.js', 'utf8'));
       bw.webContents.executeJavaScript(`(async function() { eval(await (await fetch('https://goosemod-api.netlify.app/untethered/untetheredInject.js')).text()); })();`);
     });
   }, 100);
