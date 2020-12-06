08ee6cb5a6f58f86aa6746687cabbcd37348163c
Oj <oj@oojmed.com>
2020-12-06 18:31:52 +0000

    Allow combining of branches

--- index.js
+++ index.js
@@ -376,8 +376,11 @@ let branches = {};
 const loadBranches = () => {
   const dirs = glob.sync('branches/*');
 
+  console.log('Loading branches...');
+
   for (let d of dirs) {
     const name = d.split('/').pop();
+  
     //const filePaths = glob.sync(`${d}/**/*`).filter((x) => x.match(/.*\..*$/));
     /*
     console.log(name, filePaths);
@@ -408,6 +411,42 @@ const loadBranches = () => {
       patch,
       meta: JSON.parse(patch.match(/\/\*META((.|\n)*)\*\//)[1])
     };
+
+    console.log(d, name, files, branches[name].meta);
+  }
+
+  console.log('\nCreating mixed branches...');
+
+  const branchNames = Object.keys(branches);
+
+  let combinations = [[]];
+	for (const value of branchNames) {
+		const copy = [...combinations];
+		for (const prefix of copy) {
+			combinations.push(prefix.concat(value));
+		}
+  }
+  
+  combinations = combinations.filter((x) => x.length === branchNames.length);
+
+  for (const original of combinations) {
+    const reverse = original.slice().reverse();
+
+    for (const c of [reverse, original]) {
+      const key = c.join('+');
+
+      const b = c.map((x) => branches[x]);
+
+      const res = {
+        files: b.map((x) => x.files).reduce((x, a) => a.concat(x), []),
+        patch: b.map((x) => x.patch).reduce((x, a) => `${x}\n${a}`, ''),
+        meta: {
+          version: parseInt(b.map((x) => x.meta.version).reduce((x, a) => `${x}0${a}`))
+        }
+      };
+
+      branches[key] = res;
+    }
   }
 
   console.log(branches);
