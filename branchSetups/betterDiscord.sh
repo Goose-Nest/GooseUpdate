60fd65c2749d356874eabfda917ca0d8c9eb592e
Oj <oj@oojmed.com>
2020-12-11 18:08:49 +0000

    BetterDiscord branch setup script

--- branchSetups/betterDiscord.sh
+++ branchSetups/betterDiscord.sh
@@ -1,1 +1,29 @@
 #!/bin/sh
+# This script clones (via Git) BetterDiscord's Injector into the correct directory for the branch automatically
+
+injectorDir="../branches/betterdiscord/injector"
+
+# Remove current / old Injector
+echo "Removing old injector..."
+
+rm -rf "$injectorDir"
+
+# Git clone (with no history for less data transfer / storage usage)
+echo "Cloning new injector..."
+
+git clone --depth=1 --branch=injector git@github.com:rauenzi/BetterDiscordApp.git "$injectorDir"
+
+# Remove Git data in the directory
+echo "Removing Git data..."
+
+rm -rf "$injectorDir/.git"
+
+# Remove extra unneeded files to save package size space
+echo "Removing extra unneeded files..."
+
+unneededFiles=("package.json" "README.md" ".gitignore" "LICENSE")
+
+for f in ${unneededFiles[@]}; do
+  echo $f
+  rm "$injectorDir/$f"
+done
