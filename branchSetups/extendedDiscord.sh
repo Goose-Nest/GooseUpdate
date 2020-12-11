25f6e6e419b7a77b346e75b9bc4f3d50b5b5caf8
Oj <oj@oojmed.com>
2020-12-11 18:21:07 +0000

    EnhancedDiscord branch setup script

--- branchSetups/extendedDiscord.sh
+++ branchSetups/extendedDiscord.sh
@@ -1,1 +1,24 @@
 #!/bin/sh
+# This script clones (via Git) EnhancedDiscord (their main repo) and cleans it for use in an update
+
+injectorDir="../branches/enhanceddiscord/EnhancedDiscord"
+
+# Remove current / old Injector
+echo "Removing old injector..."
+
+rm -rf "$injectorDir"
+
+# Git clone (with no history for less data transfer / storage usage)
+echo "Cloning new injector..."
+
+git clone --depth=1 git@github.com:joe27g/EnhancedDiscord.git "$injectorDir"
+
+# Remove extra unneeded files to save package size space
+echo "Removing extra unneeded files..."
+
+unneededFiles=(".git" ".eslintrc.json" "installer" ".github" "README.md" ".gitignore" "LICENSE")
+
+for f in ${unneededFiles[@]}; do
+  echo $f
+  rm -rf "$injectorDir/$f"
+done
