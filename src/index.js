15ed82d670c91a30c6cb2ee21a4b48a8359b5106
Oj <oj@oojmed.com>
2021-04-24 22:03:59 +0100

    [Chore] Bump version: v5.4.0-dev

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ const app = fastify(fastifyOptions);
 global.app = app;
 
 global.startTime = Date.now();
-global.version = '5.3.0';
+global.version = '5.4.0-dev';
 
 const port = process.argv[2] || 80;
 if (!process.argv[2]) console.log(`No port specified in args, using default: ${port}\n`);
