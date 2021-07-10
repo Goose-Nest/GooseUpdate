83b50e000f54f4f6bfa54cf66617779579864047
Oj <oj@oojmed.com>
2021-07-10 10:41:21 +0100

    [Setups > BD] Use new repo URL

--- branchSetups/betterDiscord.sh
+++ branchSetups/betterDiscord.sh
@@ -14,7 +14,7 @@ rm -f "$asarPath"
 echo "Downloading new asar..."
 
 # Based on https://gist.github.com/steinwaywhw/a4cd19cda655b8249d908261a62687f8
-curl -s https://api.github.com/repos/rauenzi/BetterDiscordApp/releases/latest \
+curl -s https://api.github.com/repos/BetterDiscord/BetterDiscord/releases/latest \
 | grep "browser_download_url.*betterdiscord.asar" \
 | cut -d '"' -f 4 \
 | wget -O "$asarPath" -qi -
